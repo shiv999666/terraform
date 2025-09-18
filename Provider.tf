6e38ec667c3d62071c652218bb9913ce97fc354d
Neolyc-Team <shiv999666@gmail.com.com>
2025-09-19 00:34:33 +0530

    First commit

---
+++ Provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "93d08993-3845-41e5-a344-95440029638a"
+}
